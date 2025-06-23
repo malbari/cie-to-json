231b42cd135ce8df10098c15b06ee632bd0de68b
Maurizio Albari <maurizio.albari@gmail.com>
2025-06-23 18:41:56 +0200

    initial commit

--- cie1.sh
+++ cie1.sh
@@ -1,1 +1,5 @@
-python cie-to-json.py ./test-images/cie1.png
+wget -c -q https://img.corrierecomunicazioni.it/wp-content/uploads/2020/10/Italian_electronic_ID_card.jpg
+
+mv "Italian_electronic_ID_card.jpg" cie1.jpg
+
+python cie-to-json.py ./cie1.jpg
--- cie2.sh
+++ cie2.sh
@@ -1,1 +1,5 @@
-python cie-to-json.py ./test-images/cie2.png
+wget -c -q https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/CIE_%28fronte%29.jpg/960px-CIE_%28fronte%29.jpg
+
+mv "960px-CIE_(fronte).jpg" cie2.jpg
+
+python cie-to-json.py ./cie2.jpg
